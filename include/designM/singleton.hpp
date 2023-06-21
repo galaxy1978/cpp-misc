cddea4acacd8094ef4c6396a24771bf1e683f4fa
galaxy_0 <17367073@qq.com>
2023-06-21 01:19:11 +0000

    update include/designM/singleton.hpp. 1  将使用static_assert判断是否是class类型，改成使用enable_if方式进行； 2  将前缀双下划线描述私有或者保护名字，改成了使用后缀双下划线来描述
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/singleton.hpp
+++ include/designM/singleton.hpp
@@ -1,8 +1,8 @@
 /**
  * @brief 单例模式
- * @version 1.1
+ * @version 1.2
  * @author 宋炜
- * @date 2023-03-08
+ * @date 2023-03-08~2023-6-21
 
  */
 
@@ -10,79 +10,52 @@
 
 #include <memory>
 #include <type_traits>
+#include "misc.hpp"
 
 namespace wheels
 {
-namespace dm
-{
-    template< typename T , bool is_ok=std::is_class<T>::value >
-    class singleton
-    {
-    protected:
-        static std::unique_ptr< T > __pt_obj;
-    protected:
-        /// 避免直接调用构造函数
-        singleton(){}
-    public:
-        singleton( const singleton& ) = delete;
-        singleton( singleton&& ) = delete;
-        singleton& operator=( const singleton& ) = delete;
-		singleton& operator=( singleton&& ) = delete;
+	namespace dm
+	{
+		template<
+			typename typeName ,
+			typename T = typename std::enable_if< std::is_class< typeName >::value , typeName >::type
+			>
+		class singleton
+		{
+		protected:
+			static std::unique_ptr< T > pt_obj__;
+		protected:
+			/// 避免直接调用构造函数
+			singleton(){}
+		public:
+			singleton( const singleton& ) = delete;
+			singleton( singleton&& ) = delete;
+			/**
+			 * @brief 工厂函数，用来创建单例对象
+			 * @return c
+			 */
+			template< typename ...Args >
+			static T * create( Args... args ){
+				if( pt_obj__ ) return pt_obj__.get();
 
-		virtual ~singleton(){}
-        /**
-         * @brief 工厂函数，用来创建单例对象
-         * @return c
-         */
-		template< typename ...Args >
-        static T * create( Args... args )
-        {
-            static_assert( is_ok , "必须使用class或者struct定义，并且包含默认的构造函数" );
-            if( __pt_obj ) return __pt_obj.get();
+				try {
+					pt_obj__.reset( new T((0,args)...) );
+				} catch ( std::bad_alloc& e ) {
+					ERROR_MSG( e.what() );
+				}
+				T * ret = pt_obj__.get();
+				return ret;
+			}
+			/**
+			 * @brief 获取当前的模块对象指针
+			 * @return 返回指针，如果模块没有初始化返回时nullptr
+			 */
+			static T * get(){
+				return pt_obj__.get();
+			}
+		};
 
-            try {
-                __pt_obj.reset( new T((0,args)...) );
-            } catch ( std::bad_alloc& e ) {
-                std::cerr << e.what() std::endl;
-            }
-            T * ret = __pt_obj.get();
-            return ret;
-        }
-        /**
-         * @brief 获取当前的模块对象指针
-         * @return 返回指针，如果模块没有初始化返回时nullptr
-         */
-        static T * get()
-        {
-            return __pt_obj.get();
-        }
-    };
-
-#define IMP_SINGLETON( type )   template<> std::unique_ptr< type > singleton< type > :: __pt_obj = {}
-}
+#define IMP_SINGLETON( type )   template<> std::unique_ptr< type > singleton< type > :: pt_obj__ = {}
+	}
 }
 
-/**
-   使用说明：
-   业务实现类应该继承于singleton,并在实现文件中使用宏 IMP_SINGLETON
-   
-   在头文件中：
-   #include "designM/singleton.hpp"
-   
-   class newSingleClass : public wheels::dm::singleton< newSingleClass > {
-	   
-	   // 您自己的实现代码
-   };
-   
-   在实现文件中：
-   IMP_SINGLETON(newSingleClass);
-   
-   // 您自己的实现代码
-   
-   应用时：
-   创建对象，
-   newSingleClass * p = newSingleClass::create();
-   
-   获取对象：
-   auto * p = newSingleClass::get();
-*/
