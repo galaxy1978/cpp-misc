87ea5587da37b96334305ca0634e40f27eca7a09
galaxy_0 <17367073@qq.com>
2023-06-30 08:56:02 +0000

    update include/designM/factory.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/factory.hpp
+++ include/designM/factory.hpp
@@ -10,9 +10,9 @@
 #include <functional>
 #include <iostream>
 
-using namespace wheels
+namespace wheels
 {
-	using namespace dm
+	namespace dm
 	{       /**
 		 * @example:
 		 *      class a : public Factory< a >{
@@ -48,10 +48,10 @@ using namespace wheels
 			};
 
 			template< typename ...Args >
-			static realType * factory( std::functional< void (Factory::emErrCode) > errfun , Args... args ){
+			static realType * factory( std::function< void (Factory::emErrCode) > errfun , Args&&... args ){
 				realType * ret = nullptr;
 				try{
-					realType * ret = new reakType( (0,args)... );
+					realType * ret = new realType( std::forward<Args>(args)... );
 				}catch( std::bad_alloc& e ){
 					std::cout << e.what() << std::endl;
 					if( errfun ){
@@ -63,66 +63,18 @@ using namespace wheels
 			}
 
 			template< typename ...Args >
-			static realType * factory( Args... args ){
+			static realType * factory( Args&&... args ){
 				realType * ret = nullptr;
 				try{
-					realType * ret = new reakType( (0,args)... );
+					realType * ret = new realType( std::forward<Args>(args)... );
 				}catch( std::bad_alloc& e ){
 					std::cout << e.what() << std::endl;
-					if( errfun ){
-						errfun( Factory::ERR_ALLOC_MEM );
-					}
 				}
 			
 				return ret;
 			}
 		};
 
-		/**
-		 * @brief 模板工厂函数
-		 * @tparam type 类名称		 
-		 * @tparam Args... ， 调用构造函数的时候构造函数的参数类型表
-		 * @param args [ I ], 构造函数的参数表
-		 * @param errfun[ I ],发生错误的回调函数
-		 * @note 这个函数接口，只能针对共有构造函数的进行工厂创建。如果存在保护的或者私有的构造函数的类则需要继承Factory
-		 *    类完成。
-		 * 
-		 * @example:
-		 *      class a{
-		 *           public:
-		 *                a();
-		 *                a( int c );
-		 *      };
-		 *
-		 *      a * pa = factory< a >( 23 );
-		 *      a * pa2 = Factory< a >( []( Factory::emErrCode err){
-		 *                      // 错误处理
-		 *                } , 23 );
-		 */
-		template< typename type , typename ...Args ,
-			  typename midType = typename std::decay< type >::type,
-			  typename realType = std::conditional<
-				  std::is_pointer< midType >::value ,
-				  typename std::remove_pointer<midType>::type,
-				  midType
-				  >
-			  >
-		realType * factory( std::functional< void (Factory::emErrCode) > errfun , Args... args ){
-			static_assert( std::is_class<realType>::value  , "this function must be used with class" );
-			static_assert( std::is_constructible<realType>::value , "class must constructible." );
-			
-			realType * ret = nullptr;
-			try{
-				realType * ret = new reakType( (0,args)... );
-			}catch( std::bad_alloc& e ){
-				std::cout << e.what() << std::endl;
-				if( errfun ){
-					errfun( Factory::ERR_ALLOC_MEM );
-				}
-			}
-			
-			return ret;
-		}
 		
 		/**
 		 * @brief 模板工厂函数
@@ -133,19 +85,18 @@ using namespace wheels
 		 */
 		template< typename type , typename ...Args ,
 			  typename midType = typename std::decay< type >::type,
-			  typename realType = std::conditional<
+			  typename realType = typename std::conditional<
 				  std::is_pointer< midType >::value ,
 				  typename std::remove_pointer<midType>::type,
 				  midType
-				  >
+				  >::type
 			  >
-		realType * factory( Args... args ){
+		realType * factory( Args&&... args ){
 			static_assert( std::is_class<realType>::value  , "this function must be used with class" );
-			static_assert( std::is_constructible<realType>::value , "class must constructible." );
 			
 			realType * ret = nullptr;
 			try{
-				realType * ret = new realType( (0,args)... );
+				realType * ret = new realType( std::forward< Args >(args)... );
 			}catch( std::bad_alloc& e ){
 				std::cout << e.what() << std::endl;
 			}
