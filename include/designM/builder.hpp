64ca8a5b1a157f5cc6c1ec236815c385626c381f
galaxy_0 <17367073@qq.com>
2023-07-04 09:44:37 +0000

    update include/designM/builder.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/builder.hpp
+++ include/designM/builder.hpp
@@ -1,21 +1,16 @@
-/**
- * @brief 建造者模式
- * @version 1.0
- * @author 宋炜
- * @date 2023-7-4
-*/
-
-#pragma once
 namespace wheels{ namespace dm {
-template< typename... Parts > class product;
+template< typename... Parts > class product{};
 
-template< typename Part1 , typename... Parts > class product : public Part1 , public product< Parts >
+template< typename Part1 , typename... Parts > 
+class product<Part1, Parts...> : public Part1 , public product< Parts... >
 {
 public:
 	virtual ~product(){}
 };
 
-template<> product{};
+
+
+template<> class product<>{};
 
 /**
  * @brief 
@@ -25,26 +20,26 @@ template< typename... Parts >
 class director 
 {
 public:
-	using product_t = product< Parts >
+	using product_t = product< Parts... >;
 
 public:
 	template< typename... Params >
 	static  std::shared_ptr< product_t > build( std::function< void ( std::shared_ptr< product_t > ) >fun ){
 		std::shared_ptr< product_t >   pt_product;
 		try{
-			pt_product.reset( new product );
+			pt_product.reset( new product_t );
 			if( fun ){
 				fun( pt_product );
 			}
 		}catch( std::bad_alloc& e ){
 			std::cout << e.what() << std::endl;		
 			if( pt_product ){
-				pt_product.reset( nullptr );
+				pt_product.reset(  );
 			}
 		}catch( std::runtime_error& e  ){
 			std::cout << e.what() << std::endl;
 			if( pt_product ){
-				pt_product.reset( nullptr );
+				pt_product.reset( );
 			}
 		}
 		
