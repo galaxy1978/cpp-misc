e30a01749b955c1e2e2db7ce9fd94a689046a3ec
galaxy_0 <17367073@qq.com>
2023-11-06 08:05:32 +0000

    update include/designM/state.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/state.hpp
+++ include/designM/state.hpp
@@ -70,7 +70,7 @@ public:
         virtual ~colleague(){}
         /**
          * @brief 重载recv函数，并在其中针对tuple解包，调用回调函数通知状态变化
-         * @param tpl
+         * @param tpl[ I ]，
          */
         virtual void recv( const std::tuple< STATE_TYPE , private__::emEvent , CONDITION_DATA_TYPE >& tpl ) final{
 			if( m_cb__ ){
@@ -102,28 +102,52 @@ protected:
 public:
 
     state():m_is_running__( false ){
-        pt_producer__ = std::make_shared<colleague>();
-    }
-	virtual ~state(){}
+        // 准备中介者模式进行中间消息传递操作
+        pt_mdt__ = std::make_shared<mediator_t>();
 
-	void on( std::function< void () > fun ){
+        pt_producer__ = std::make_shared<colleague>();
+        pt_consumer__ = std::make_shared<colleague>();
+
+        pt_mdt__->add( pt_producer__ );
+        pt_mdt__->add( pt_consumer__ );
+    }
+
+    virtual ~state(){
+        if( pt_mdt__ ){
+            pt_mdt__->run( false );
+        }
+    }
+    /**
+     * @brief 登记状态转换动作函数
+     * @param fun[ I ]，动作函数
+     */
+    void on( std::function< void (stateData_t , private__::emEvent , CONDITION_DATA_TYPE) > fun ){
 		pt_consumer__->m_cb__ = fun;
-	}
-	
+    }
+    /**
+     * @brief 指定起始状态
+     * @param data[ I ],状态
+     */
 	void setStart( const stateData_t& data ){
 		m_start__ = data;
 		if( m_is_running__ == false ){
 			m_current__ = data;
 		}
 	}
-
+    /**
+     * @brief 指定结束状态
+     * @param data[ I ],状态
+     */
     void setEnd( const stateData_t& data ){
         m_end__ = data;
         if( m_is_running__ == false ){
             m_current__ = data;
         }
     }
-
+    /**
+     * @brief 启动
+     * @param sw
+     */
 	void start( bool sw ){
 		if( m_is_running__ == sw ){
 			return;
@@ -131,15 +155,22 @@ public:
 
         m_is_running__ = sw;
         if( sw ){
+            pt_mdt__->run( true );
             m_current__ = m_start__;
             pt_producer__->send( pt_consumer__ ,
                                 m_end__ ,
                                 private__::emEvent::EVT_READY ,
                                 std::is_arithmetic<CONDITION_DATA_TYPE>::value?CONDITION_DATA_TYPE():(CONDITION_DATA_TYPE)0.0 );
+        }else{
+             pt_mdt__->run( false );
         }
 	}
-
+    /**
+     * @brief 执行一次无参数的转换
+     * @return 成功操作返回true;否则返回false
+     */
     bool execute(){
+        if(m_is_running__ == false ) return false;
         auto it = m_arcs__.find( m_current__ );
         if( it != m_arcs__.end() ){
             call_leave__( it->m_second[0], std::is_arithmetic<CONDITION_DATA_TYPE>::value ? CONDITION_DATA_TYPE() : (CONDITION_DATA_TYPE)0.0 );
@@ -156,6 +187,8 @@ public:
      * @return 成功操作返回true;否则返回false
      */
     bool execute( const stateData_t& data ){
+        if(m_is_running__ == false ) return false;
+
 		auto it = m_arcs__.find( m_current__ );
 		if( it != m_arcs__.end() ){
 			bool find_dst = false;
@@ -184,18 +217,27 @@ public:
 
         return true;
 	}
-
+    /**
+     * @brief 删除状态
+     * @param state
+     * @return
+     */
     bool addState( const stateData_t& state ){
+        if(m_is_running__ == true ) return false;
         private__::stStat<stateData_t> s( state );
         auto rst = m_stats__.insert( std::make_pair(state , s ));
         return rst.second;
 	}
-
+    /**
+     * @brief 添加状态
+     * @param state
+     */
 	void removeState( const stateData_t& state ){
+        if(m_is_running__ == true ) return;
         auto it = std::find( m_stats__.begin() , m_stats__.end() , state );
         m_stats__.erase( it );
 
-        // 清理状态起始ARM
+        // 清理状态起始ARC
         auto it1 = m_arcs__.find( state );
         if( it1 ){
             m_arcs__.erase( it1 );
@@ -210,16 +252,27 @@ public:
             }
         }
 	}
-
+    /**
+     * @brief 添加转换
+     * @param from
+     * @param to
+     * @param cnd
+     */
     void addArc( const stateData_t& from , const stateData_t& to , std::function< bool ( const CONDITION_DATA_TYPE& ) > cnd ){
+        if(m_is_running__ == true ) return;
         auto it = m_arcs__.find( from );
         if( it ){
             private__::stArc<STATE_TYPE,CONDITION_DATA_TYPE> item( from , to , cnd );
             it->second.insert( item );
         }
 	}
-
+    /**
+     * @brief 移除转换
+     * @param from
+     * @param to
+     */
     void removeArc( const stateData_t& from , const stateData_t& to ){
+        if(m_is_running__ == true ) return;
         auto it = m_arcs__.find( from );
         if( it ){
             for( auto it1 = it->second.begin(); it != it->end(); it ++ ){
