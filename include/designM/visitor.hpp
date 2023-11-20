678fd670d0262a0283aad09dcdc687d68e3016ae
galaxy_0 <17367073@qq.com>
2023-11-20 03:22:19 +0000

    update include/designM/visitor.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/visitor.hpp
+++ include/designM/visitor.hpp
@@ -20,12 +20,21 @@ protected:
 public:
     vistor(){}
     virtual ~vistor(){}
-	
+	/**
+	 * @brief 添加处理方法
+	 * @param name[ I ], 方法名称
+	 * @param func,方法函数对象
+	 * @return 如果成功添加方法，返回true，否则返回false
+	*/
     bool addMethod( const std::string& name , func_t func ){
 		auto rst = m_funcs__.insert( std::make_pair( name , func ) );
 		return rst.second;
 	}
-	
+	/**
+	 * @brief 移除方法
+	 * @param 方法名称
+	 * @return 如果没有找到方法，返回false,否则删除方法返回true
+	*/
     bool eraseMethod( const std::string& name ){
 		auto it = m_funcs__.find( name );
 		if( it ){
@@ -35,7 +44,21 @@ public:
 		
 		return false;
 	}
-	
+	/**
+	 * @brief 这是一个方便操作接口，可以用过 vistor["abc"]( data )调用处理数据
+	*/
+	func_t operator[]( const std::string& name ){
+		auto it = m_funcs__.find( name );	
+		if( it ){
+			return it->second;
+		}
+		return {};
+	}
+	/**
+	 * @brief 调用方法处理数据
+	 * @param name[ I ], 方法名称
+	 * @param 要处理的数据
+	*/
 	RET call( const std::string& name, dataItem_t& data ){
 		auto it = m_funcs__.find( name );	
 		if( it ){
@@ -45,6 +68,11 @@ public:
 		return {};
 	}
 	
+	/**
+	 * @brief 调用方法处理批量数据
+	 * @param name[ I ], 方法名称
+	 * @param 要处理的数据
+	*/
 	template< typename InputIterator >
 	void callEach( const std::string& name , InputIterator begin , InputIterator end ){
 		auto it = m_funcs__.find( name );
