7484a318ce2454dbc822dd37e4b94e61f9ef58c7
galaxy_0 <17367073@qq.com>
2023-11-20 03:34:58 +0000

    add example/visitor.cpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

---
+++ example/visitor.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include "designM/vistor.hpp"
+
+// 自定义数据类型
+struct Data {
+  Data(int value) : value(value) {}
+  int value;
+};
+
+// 处理函数1
+int func1(Data& data) {
+  std::cout << "func1 called with value: " << data.value << std::endl;
+  return data.value + 1;
+}
+
+// 处理函数2
+int func2(Data& data) {
+  std::cout << "func2 called with value: " << data.value << std::endl;
+  return data.value * 2;
+}
+
+int main() {
+  // 创建vistor对象，并指定数据类型为Data，返回类型为int
+  vistor<Data, int> myVistor;
+
+  // 添加方法到vistor对象
+  myVistor.addMethod("func1", func1);
+  myVistor.addMethod("func2", func2);
+
+  // 创建数据对象
+  Data myData(10);
+
+  // 调用指定名称的方法处理数据
+  int result1 = myVistor["func1"](myData);
+  std::cout << "Result1: " << result1 << std::endl;
+
+  int result2 = myVistor["func2"](myData);
+  std::cout << "Result2: " << result2 << std::endl;
+
+  // 批量处理数据
+  std::vector<Data> dataVec = {Data(20), Data(30)};
+  myVistor.callEach("func1", dataVec.begin(), dataVec.end());
+
+  return 0;
+}
