7d857b44331dd0d9a93277465d27c7e73d916f0e
galaxy_0 <17367073@qq.com>
2023-11-29 07:46:51 +0000

    update include/designM/facade.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/facade.hpp
+++ include/designM/facade.hpp
@@ -7,26 +7,39 @@
 
 #pragma once
 
+#include <functional>
+
+namespace facade_private__ {
+    template < typename itfcType , typename... implType >
+    struct TYPE_CHK_HELPER__{};
+
+    template< typename itfcType , typename implType1 , typename... implType >
+    struct TYPE_CHK_HELPER__< itfcType , implType1 , implType...>
+    {
+        static_assert( std::is_base_of< itfcType , implType1 >::value , "" );
+    };
+
+    template< typename itfcType >
+    struct TYPE_CHK_HELPER__<itfcType> {};
+}
+
 namespace wheels{ namespace dm {
-template <typename retType, typename... subTypes>
-struct facadeInh{};
+// 以继承的方式实现
+template <typename retType, typename... subTypes> struct facadeInh{};
 
 template <typename retType, typename subType1, typename... subTypes>
-struct facadeInh<retType, subType1, subTypes...> : public subType1, public facadeInh<retType, subTypes...>
-{
-};
+struct facadeInh<retType, subType1, subTypes...> : public subType1, public facadeInh<retType, subTypes...>{};
 
 template <typename retType>
 struct facadeInh<retType>
 {
 	template <typename... Args>
-    retType run(std::function<retType(facadeInh<retType> * )> func)
-    {
-        return func(this);
-    }
+	retType run(std::function<retType(facadeInh<retType> * )> func)	{
+		return func(this);
+	}
 };
 
-// 下面是方式2, 以接口的方式实现
+// 下面是方式2， 以接口的方式实现
 // -----------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------
 #define FACADE_START_DECLARE_SUB_ITFC( name ) \
@@ -37,12 +50,15 @@ struct name{ \
 
 #define FACADE_ADD_ITFC( RET , NAME , ... ) \
 virtual RET NAME( __VA_ARGS__ ) = 0;
-	
+
 
 template< typename itfcType ,  typename... subTypes >
 class facadeItfc
 {
 public:
+    using itfc_t = typename std::remove_pointer< typename std::decay< itfcType >::type >::type;
+public:
+
 	template< int N , typename tupleParams >
 	struct INIT_HELPER__
 	{
@@ -55,10 +71,12 @@ public:
 	
 	template< typename tupleParams >
 	struct INIT_HELPER__<0 , tupleParams >{
-		static void init__( std::vector< itfcType * >& vec , tupleParams& t ){}
+        static void init__( std::vector< itfcType * >&  , tupleParams& ){}
 	};
 protected:
-    std::vector< itfcType * >  m_subs__;
+    std::vector< itfc_t * >  m_subs__;
+
+    facade_private__::TYPE_CHK_HELPER__<itfc_t , subTypes... >   m_chker__[0];
 public:
     facadeItfc(subTypes*... subs):m_subs__( sizeof...(subTypes) ) {
 		std::tuple< subTypes*... > t = std::make_tuple( subs... );
