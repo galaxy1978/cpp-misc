26c2f55a1e0335eb22aaae05ac7dcf0fd8d3d4ce
galaxy_0 <17367073@qq.com>
2023-11-29 07:35:46 +0000

    update include/designM/composite.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/composite.hpp
+++ include/designM/composite.hpp
@@ -10,7 +10,7 @@
 #include <type_traits>
 #include <functional>
 #include <vector>
-#include <stdexcept>
+#include <stack>
 #include <memory>
 
 namespace wheels
@@ -23,10 +23,13 @@ namespace dm{
 		using type_t = typename std::remove_pointer<typename std::decay< ITFC_TYPE >::type >::type;
         using composite_t = composite< type_t >;
 		using children_t = std::vector< std::shared_ptr< composite< type_t > > >;
-		using iterator = children_t::iterator;
+        using iterator = typename children_t::iterator;
 	public:
 		composite( ): p_obj__( nullptr ){}
 		composite( std::shared_ptr< type_t > p ): p_obj__( p ){}
+		composite( type_t&& data ){
+            p_obj__ = std::make_shared<type_t>( data );
+		}
 		virtual ~composite(){}
 		/**
 		 * @brief 判断是否是叶子节点
@@ -50,8 +53,30 @@ namespace dm{
 				m_children__.erase( it );
 			}
 		}
-		
-		void for_each( std::function< void ( composite_t * item ) > fun ){
+        /**
+         * @brief 先序遍历操作
+         * @param func[ I ]， 回调函数，返回true继续遍历，否则结束遍历操作
+         */
+        void preOrderTraversal(std::function<bool (type_t*)> func) {
+            std::stack<iterator> stack;
+            stack.push(m_children__.begin());
+            while (!stack.empty()) {
+                auto it = stack.top();
+                stack.pop();
+
+                if( !func(it->get()) ) break;
+
+                if (!it->get()->isLeaf()) {
+                    stack.push(it->begin());
+                    stack.push(std::next(it->begin()));
+                }
+            }
+        }
+        /**
+         * @brief 本层遍历
+         * @param fun
+         */
+        void for_each( std::function< void ( type_t * ) > fun ){
 			for( auto item : m_children__ ){
 				bool rst = fun( item.get() );
 				if( !rst ) break;
@@ -59,8 +84,7 @@ namespace dm{
 		}
 		
 		type_t * get(){ return p_obj__.get(); }
-		
-		
+
 	protected:
 		children_t   m_children__;
 
