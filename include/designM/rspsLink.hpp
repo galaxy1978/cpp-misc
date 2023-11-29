3eab7bfc84a08ab2293e3540d093216724418e3e
galaxy_0 <17367073@qq.com>
2023-11-29 09:55:25 +0000

    update include/designM/rspsLink.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/rspsLink.hpp
+++ include/designM/rspsLink.hpp
@@ -6,202 +6,57 @@
  */
 
 #pragma once
-#include <assert.h>
-#include <functional>
+
 #include <list>
-/// 使用示例：
-///
-/// class myItfc{
-///    virtual int do_something( int ) = 0;
-///    定义接口
-/// };
 
-/// class myItfc1 : public myItfc{
-///      接口实现1
-/// };
-
-/// class myItfc1 : public myItfc{
-///      接口实现2
-/// };
-
-/// class myRsps : public rspsLink< myItfc >{
-///        自己的定义和方法
-/// }
-
-/// myRsps rsps;
-/// rsps.push_back( new myItfc1 );
-/// rsps.push_back( new myItfc2 );
-/// rsps.push_back( ... );
-///
-/// 按照责任链传递操作
-/// int a = 1;
-/// rsps.forward([&]( myRsps::stLinkItem& item ){
-///       a = item->do_something( a );
-/// });
 namespace wheels
 {
 namespace dm
 {
-	template<typename itfcType >
+    template< typename ...Params >
 	class rspsLink
 	{
 	public:
-		using interface_type = itfcType;
-		
-		struct stLinkItem{
-			mutable interface_type    * p_itfc;
-			
-			interface_type * operator->(){ return p_itfc; }
-			stLinkItem():p_itfc( nullptr ){}
-			stLinkItem( interface_type * itfc ):p_itfc( itfc ){}
-			stLinkItem( const stLinkItem& b ):p_itfc( b.p_itfc ){}
-			stLinkItem( stLinkItem&& b ):p_itfc( b.p_itfc ){}
-
-			stLinkItem& operator=( stLinkItem&& b ){
-				p_itfc = b.p_itfc;
-				return *this;
-			}
-		};
-
-		using link_t =  std::list< stLinkItem >;
-		using iterator = typename std::list< stLinkItem >::iterator; 
+        struct itfc
+        {
+            virtual bool operation( Params&& ... args ) = 0;
+        };
+        using itfc_t = itfc;
+        using link_t =  std::list< itfc * >;
+        using iterator = typename link_t::iterator;
 	protected:
-		link_t             __m_link;
-		mutable iterator   __m_curr_it;
-		bool               __m_started;
-		bool               __m_is_end;
+        link_t             m_link__;
 	public:
-		rspsLink():__m_started( false ){}
+        rspsLink(){}
 		virtual ~rspsLink(){}
 
-		/**
-		 * @brief 执行一次链式传递,从链头部传递到尾部
-		 * @param cb 传递过程的处理方法
-		 * @return 成功调用返回true，否则返回false
-		 * @note 当传递到最后的时候，函数会返回false同时将迭代器位置设为链首位置。
-		 *   实际在应用的时候在回调函数中调用自定义的接口进行数据传递,例如：
-		 *
-		 *  forward( [=](rspsLink :: stLinkItem& curr ){
-		 *         curr->do_something( ... );
-		 *  });
-		 */
-		bool forward( std::function< void ( stLinkItem& curr ) > cb ){
-			bool ret = true;
-			
-			if( __m_link.size() == 0 ) return false;
-			if( __m_curr_it == __m_link.end() ){
-				__m_is_end = true;
-				__m_started = false;
-				__m_curr_it = __m_link.begin();
-				return false;
-			}
-			if( !cb ) return false;
-			if( __m_started == false ){
-				__m_started = true;
-				__m_is_end = false;
-				__m_curr_it = __m_link.begin();
-			}
-			
-			cb( *__m_curr_it );
-			__m_curr_it ++;
-			
-			return ret;
-		}
-
-		bool isEnd(){ return __m_is_end; }
-		/**
-		 * @brief 执行一次链式传递,从链尾部传递到头
-		 * @param cb 传递过程的处理方法
-		 * @return 成功调用返回true，否则返回false
-		 * @note 当传递到最后的时候，函数会返回false同时将迭代器位置设为链首位置。
-		 *   实际在应用的时候在回调函数中调用自定义的接口进行数据传递,例如：
-		 *
-		 *  backward( [=](rspsLink :: stLinkItem& curr ){
-		 *         curr->do_something( ... );
-		 *  });
-		 */
-		bool backward( std::function< void ( stLinkItem &curr ) > cb ){
-			bool ret = true;
-			if( __m_link.size() == 0 ) return false;
-			if( __m_curr_it == __m_link.rend() ){
-				__m_curr_it = __m_link.rend();
-				__m_curr_it ++;
-				__m_is_end = true;
-				__m_started = false;
-				return false;
-			}
-			
-			if( __m_started == false ){
-				__m_started = true;
-				__m_is_end = false;
-				__m_curr_it = __m_link.rbegin();
-			}
-			
-			if( !cb ) return false;
-
-			cb( *__m_curr_it );
-			__m_curr_it ++;
-
-            return ret;
-		}
-		/**
-		 * 在指定位置之前添加处理接口
-		 */
-		iterator insert( iterator it , interface_type * rsps ){
-			assert( rsps );
-			stLinkItem item( rsps );
-			iterator ret = __m_link.insert( it , item ) ;
-
-			return ret;
-		}
+        void request( Params&&... params ){
+            for( auto item : m_link__ ){
+                bool rst = item->operation( std::forward<Params>( params)...);
+                if( rst == false ){
+                    break;
+                }
+            }
+        }
 		/**
 		 * @brief 添加处理接口
 		 * @param 处理接口对象指针
 		 */
-		void push_back( interface_type * rsps ){
-			assert( rsps );
-			stLinkItem item( rsps );
-			__m_link.push_back( item );
+        void push_back( itfc_t * rsps ){
+            m_link__.push_back( rsps );
 		}
 		
-		/**
-		 * @brief 移除尾部对象
-		 */
-		stLinkItem&& pop_back(){
-			stLinkItem item = __m_link.back();
-
-			__m_link.pop_back();
-			
-			return std::move( item );
-		}
-
 		void erase( iterator it ){
-			__m_link.erase( it );
+            m_link__.erase( it );
 		}
 		
 		void clear(){
-			__m_link.erase( __m_link.begin() , __m_link.end() );
+            m_link__.erase( m_link__.begin() , m_link__.end() );
 		}
 
-		bool setFromFront(){
-			if( __m_link.size() == 0 ) return false;
 
-			__m_curr_it = __m_link.begin();
-
-			return true;
-		}
-
-		bool setFromEnd(){
-			if( __m_link.size() == 0 ) return false;
-
-			__m_curr_it = __m_link.end();
-
-			return true;
-		}
-
-		iterator begin(){ return __m_link.begin(); }
-		iterator end(){ return __m_link.end(); }
+        iterator begin(){ return m_link__.begin(); }
+        iterator end(){ return m_link__.end(); }
 		
 	};
-}
-}
+}}
