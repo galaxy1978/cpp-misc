795bf8c5e7d80af3e717102271d1e559a6fb541e
galaxy_0 <17367073@qq.com>
2023-11-29 08:22:01 +0000

    update include/designM/builder.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/builder.hpp
+++ include/designM/builder.hpp
@@ -11,6 +11,7 @@
 #include <memory>
 #include <functional>
 #include <stdexcept>
+#include <type_traits>
 namespace wheels{ namespace dm {
 #if BUILD_USE_TUPLE == 1
 #include <tuple>
@@ -18,70 +19,72 @@ template< itfcType , typename ...Parts >
 class product
 {
 protected:
-	std::tuple< Parts... >   m_parts__;
+    std::tuple< Parts... >   m_parts__;
 public:
-	virtual ~product(){}
-	
-	template< int N >
-	auto get()->decltype(typename std::tuple_element< N , prdtTypes >::type){
-		return std::get< N >( m_parts__ );
-	}
-	
-	template< int idx , typename PART_TYPE >
-	void set( PART_TYPE param ){
-		std::get< idx >( m_parts__ ) = param;
-	}
+    virtual ~product(){}
+
+    template< int N >
+    auto get()->decltype(typename std::tuple_element< N , prdtTypes >::type){
+        return std::get< N >( m_parts__ );
+    }
+
+    template< int idx , typename PART_TYPE >
+    void set( PART_TYPE param ){
+        std::get< idx >( m_parts__ ) = param;
+    }
 };
 #else
 template< typename itfcType , typename... Parts > class product{};
 
-template< typename itfcType , typename Part1 , typename... Parts > 
+template< typename itfcType , typename Part1 , typename... Parts >
 class product<itfcType , Part1 , Parts... > : public Part1 , public product< itfcType , Parts... >
 {
 public:
-	virtual ~product(){}
+    virtual ~product(){}
 };
 
 
-template< typename itfcType > 
+template< typename itfcType >
 class product<itfcType>: public itfcType{
 public:
-	virtual ~product(){}
+    virtual ~product(){}
 };
 #endif
 /**
- * @brief 
+ * @brief
  * @tparam Parts ，组件表
 */
-template< typename itfcType , typename... Parts >
-class director 
+template< typename pdtItfcType , typename... Parts >
+class director
 {
 public:
-	using product_t = product< itfcType , Parts... >;
+    using itfc_t = typename std::remove_pointer< typename std::decay< pdtItfcType >::type >::type;
+    using product_t = product< itfc_t , Parts... >;
 
 public:
-	template< typename... Params >
-	static  std::shared_ptr< product_t > 
-	build( std::function< void ( std::shared_ptr< product_t > ) >fun ){
-		std::shared_ptr< product_t >   pt_product;
-		try{
-			pt_product.reset( new product_t );
-			if( fun ){
-				fun( pt_product );
-			}
-		}catch( std::bad_alloc& e ){
-			std::cout << e.what() << std::endl;		
-			if( pt_product ){
-				pt_product.reset(  );
-			}
-		}catch( std::runtime_error& e  ){
-			std::cout << e.what() << std::endl;
-			if( pt_product ){
-				pt_product.reset( );
-			}
-		}
-		
-		return pt_product;
-	}
+    director(){}
+    virtual ~director(){}
+
+    template< typename... Params >
+    static  std::shared_ptr< product_t >
+    build( std::function< void ( std::shared_ptr< product_t > ) >fun ){
+        std::shared_ptr< product_t >   pt_product;
+        try{
+            pt_product.reset( new product_t );
+            if( fun ){
+                fun( pt_product );
+            }
+        }catch( std::bad_alloc& e ){
+            if( pt_product ){
+                pt_product.reset(  );
+            }
+        }catch( std::runtime_error& e  ){
+            if( pt_product ){
+                pt_product.reset( );
+            }
+        }
+
+        return pt_product;
+    }
 };
 }}
