82612b664af72d43fce87de34715fac14bc11849
galaxy_0 <17367073@qq.com>
2023-12-08 08:25:09 +0000

    update example/memo.cpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- example/memo.cpp
+++ example/memo.cpp
@@ -1,4 +1,10 @@
-#include "memo.hpp"
+#include <iostream>
+
+#include "designM/memo.hpp"
+
+using namespace wheels;
+using namespace dm;
+
 int main() {
     using crtk = caretaker<int>;
     crtk caretaker;
@@ -6,20 +12,20 @@ int main() {
 
     // 设置初始状态
     int state = 5;
-    originator.setState(&state);
+    originator.setState(state);
 
-    std::cout << "初始状态: " << *originator.getState() << std::endl;
+    std::cout << "init state: " << originator.getState() << std::endl;
 
     // 创建备忘录并保存到caretaker中
     caretaker.add(originator.createMemento());
 
     // 修改状态
-    *originator.getState() = 10;
-    std::cout << "修改后的状态: " << *originator.getState() << std::endl;
+    originator.getState() = 10;
+    std::cout << "updated state: " << originator.getState() << std::endl;
 
     // 使用备忘录恢复原始状态
     originator.restoreMemento(caretaker.get(0));
-    std::cout << "恢复后的状态: " << *originator.getState() << std::endl;
+    std::cout << "restored: " << originator.getState() << std::endl;
 
     return 0;
 }
