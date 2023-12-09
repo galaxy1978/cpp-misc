a15310bbfa4ef60d28c8e03e92d596b526853039
galaxy_0 <17367073@qq.com>
2023-12-09 07:11:03 +0000

    update include/designM/rspsLink.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/rspsLink.hpp
+++ include/designM/rspsLink.hpp
@@ -9,31 +9,45 @@
 
 #include <list>
 
+namespace private__
+{
+	struct respItfc__{};
+}
+
+#define DECLARE_RESPLINK_ITFC( name )    \
+	struct name :public private__::respItfc__ {
+		
+#define RESPLINK_ITFC_MTD( ... )   virtual bool operation(  __VA_ARGS__ ) = 0;
+
+#define END_DECLARE_RESPLINK_ITFC()   };
+	
+	
 namespace wheels
 {
 namespace dm
 {
-    template< typename ...Params >
+    template< typename ITFC_TYPE >
 	class rspsLink
 	{
 	public:
-        struct itfc
-        {
-            virtual bool operation( Params&& ... args ) = 0;
-        };
-
-        using itfc_t = itfc;
-        using link_t =  std::list< itfc * >;
+		using itfc_t = typename std::remove_pointer< typename std::decay< ITFC_TYPE >::type >::type;
+		static_assert( std::is_base_of< private__::respItfc__ ,  itfc_t >::value , "" );
+        
+        using link_t =  std::list< itfc_t * >;
         using iterator = typename link_t::iterator;
 	protected:
         link_t             m_link__;
 	public:
         rspsLink(){}
 		virtual ~rspsLink(){}
-
+		
+		template< typename ...Params >
         void request( Params&&... params ){
             for( auto item : m_link__ ){
-                bool rst = item->operation( std::forward<Params>( params)...);
+				auto * p = dynamic_cast< itfc_t *>( item );
+				if( p == nullptr ) continue;
+				
+                bool rst = p->operation( std::forward<Params>( params)...);
                 if( rst == false ){
                     break;
                 }
@@ -42,7 +56,7 @@ namespace dm
 
         template< typename subType >
         void push_back( subType * rsps ){
-            static_assert( std::is_base_of<itfc_t , subType >::value , "" );
+            static_assert( std::is_base_of<private__::respItfc__ , subType >::value , "" );
             m_link__.push_back( rsps );
         }
 
