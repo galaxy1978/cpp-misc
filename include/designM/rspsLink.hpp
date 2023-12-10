63381202ca9363ff9729c7543a645b2642369b69
galaxy_0 <17367073@qq.com>
2023-12-10 10:04:14 +0000

    update include/designM/rspsLink.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/rspsLink.hpp
+++ include/designM/rspsLink.hpp
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <list>
+#include <type_traits>
 
 namespace private__
 {
@@ -33,7 +34,7 @@ namespace dm
 		using itfc_t = typename std::remove_pointer< typename std::decay< ITFC_TYPE >::type >::type;
 		static_assert( std::is_base_of< private__::respItfc__ ,  itfc_t >::value , "" );
         
-        using link_t =  std::list< itfc_t * >;
+        using link_t =  std::list<  itfc_t* >;
         using iterator = typename link_t::iterator;
 	protected:
         link_t             m_link__;
@@ -44,7 +45,7 @@ namespace dm
 		template< typename ...Params >
         void request( Params&&... params ){
             for( auto item : m_link__ ){
-				auto * p = dynamic_cast< itfc_t *>( item );
+				auto * p = item ;
 				if( p == nullptr ) continue;
 				
                 bool rst = p->operation( std::forward<Params>( params)...);
@@ -53,21 +54,44 @@ namespace dm
                 }
             }
         }
+		
+		template< typename Func_t , typename... Params >
+		void requestCallback( Func_t && fun , Params&& ...args ){
+			using result_type = typename std::result_of< Func_t( itfc_t* , Params&&... ) >::type;
+			static_assert( std::is_same<result_type , bool >::value , "" );
+			
+			for( auto item : m_link__ ){
+				auto * p =  item;
+				if( p == nullptr ) continue;
+				
+				bool rst = fun( p , std::forward<Params>(args)... );
+				if( rst == false ){
+                    break;
+                }
+			}
+		}
 
         template< typename subType >
-        void push_back( subType * rsps ){
-            static_assert( std::is_base_of<private__::respItfc__ , subType >::value , "" );
+        void push_back(  subType * rsps ){
+			using type = typename std::remove_pointer< typename std::decay<subType>::type > :: type;
+            static_assert( std::is_base_of<private__::respItfc__ , type >::value , "" );
             m_link__.push_back( rsps );
         }
+		
+		template< typename subType >
+		void insert( iterator it , subType * rsps ){
+			using type = typename std::remove_pointer< typename std::decay<subType>::type > :: type;
+            static_assert( std::is_base_of<private__::respItfc__ , type >::value , "" );
+            m_link__.insert( it , rsps );
+		}
 
 		void erase( iterator it ){
             m_link__.erase( it );
 		}
 		
-		void clear(){
-            m_link__.erase( m_link__.begin() , m_link__.end() );
+		void erase( iterator start , iterator end ){
+            m_link__.erase( start , end );
 		}
-
 
         iterator begin(){ return m_link__.begin(); }
         iterator end(){ return m_link__.end(); }
