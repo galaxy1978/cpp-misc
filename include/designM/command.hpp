7b29443d650e1ba8c786512ec2f6cc5fa9b69b91
galaxy_0 <17367073@qq.com>
2023-12-16 02:55:37 +0000

    update include/designM/command.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/command.hpp
+++ include/designM/command.hpp
@@ -9,6 +9,10 @@
 
 #include <string>
 
+#if !defined( CMD_USE_SINGLETON )
+#	define CMD_USE_SINGLETON   (1)
+#endif
+
 #include "designM/eventDetail/event.hpp"
 #include "designM/eventDetail/dispatcher.hpp"
 #include "designM/eventDetail/mainloop.hpp"
@@ -17,7 +21,7 @@ namespace wheels
 {
 	namespace dm
 	{
-		using commandUI = mainloop< uint32_t >;
+		using commandUI = mainLoop< uint32_t >;
 		using commandStr = mainLoop< std::string >;
 	}
 }
