ede8208f198cb734ab99a653ea137e7d15034e0e
galaxy_0 <17367073@qq.com>
2024-01-07 10:20:53 +0000

    update include/designM/decorator.hpp.
    Signed-off-by: galaxy_0 <17367073@qq.com>

--- include/designM/decorator.hpp
+++ include/designM/decorator.hpp
@@ -1,87 +1,131 @@
 /**
  * @brief 装饰器模式
- * @version 1.0
+ * @version 1.1
  * @author 宋炜
  * @date 2023-4-23
+ * 
+ *
+ * 2024-1-7 新增接口约束方式；新增接口声明宏 
  */
 
 #pragma once
-
 #include <type_traits>
 #include <vector>
+#include <functional>
+
+namespace decorator_private__
+{
+	struct stDecorateeItfc__{
+		virtual ~stDecorateeItfc__(){}
+	};
+}
+
+#define BEGIN_DECLARE_DECORTEE_ITFC( name )							\
+	struct name: public  decorator_private__::stDecorateeItfc__{	\
+		virtual ~name(){}
+		
+#define DECORTEE_METHOD( RET , NAME , __VA_ARGS__ )    				\
+	virtual RET NAME( __VA_ARGS__ ) = 0
+	
+#define END_DECLARE_DECORTEE_ITFC
+
 
 namespace wheels
 {
 	namespace dm
 	{
 		template< typename itfcType >
-		class decorator
+		class decorator : public  decorator_private__::stDecorateeItfc__
 		{		
 		public:
-			template< typename itfcType >
+            using itfc_t = typename std::remove_pointer< typename std::decay<itfcType>::type >::type;
+			static_assert( std::is_base_of<decorator_private__::stDecorateeItfc__>::value , "" );
+			/**
+			 * @brief 装饰品包装，类似与化妆品包装盒
+			 */
 			class decoratee 
 			{
-			public:
-				imp_ptr = std::shared_ptr< itfcType >;
 			protected:
-				imp_ptr pt_imp__;  
+                itfc_t  * p_imp__;     // 实际的装饰品，类似实际的化妆品
 			public:
-				decoratee():pt_imp__( nullptr ){}
-				decoratee( imp_ptr imp ):pt_imp__( imp ){}
+                decoratee():p_imp__( nullptr ){}
+                decoratee( itfcType * imp ):p_imp__( imp ){}
+                decoratee( itfcType&& b ): p_imp__( b.p_imp__ ){}
 
-				decoratee& operator=( itfcType& b ){
-					pt_imp__ = b.pt_imp__;
+				decoratee& operator=( itfcType&& b ){
+                    p_imp__ = b.p_imp__;
 					return *this;
 				}
 			
-				imp_ptr operator->(){ return pt_imp__; }
-				itfcType& operator*(){ return *pt_imp__; }
-				void set( imp_ptr imp ){ pt_imp__ = imp; }
-				imp_ptr get(){ return pt_imp__; }
+                itfc_t * operator->(){ return p_imp__; }
+
+                void set( itfc_t * imp ){ p_imp__ = imp; }
+                itfc_t * get(){ return p_imp__; }
 			};
 
-			using dcrte_t = decoratee< itfcType > ;
+			// 装饰品数据类型包装数据类型
+            using dcrte_t = decoratee;
+			// 装饰品
 			using data_t = std::vector< dcrte_t >;
-			using iterator = data_t :: iterator;
+            using iterator = typename data_t :: iterator;
 		protected:
-			data_t m_dcrtes__;
+            data_t m_dcrtes__;
 		public:
 			decorator(){}
 			virtual ~decorator(){}
 
-			size_t decrat( decrat_t& dcrtee ){
-				m_dcrtes__.push_back( decrat_t );
-				return m_dcrtes__.size() - 1;
+			/**
+			 * @brief 添加装饰品.
+			 * @param dcrtee[ I ], 装饰品对象
+			 * @return 装饰id,也就是索引号。
+			 * @note 添加装饰的时候返回的是其索引，如果后续修改了需要同步操作
+			 */
+            size_t decrat( dcrte_t& dcrtee ){
+                m_dcrtes__.push_back( dcrtee );
+                return m_dcrtes__.size() - 1;
 			}
-
+			/**
+			 * @brief 通过接口类型添加装饰品
+			 * @param dcrtee
+			 * @return
+			 */
 			size_t decrat( itfcType * dcrtee ) {
-				m_dcrtes__.push_back( decrat_t( dcrtee ) );
-				return m_dcrtes__.size() - 1;
+                m_dcrtes__.push_back( decrat_t( dcrtee ) );
+                return m_dcrtes__.size() - 1;
 			}
 			
+			/**
+			 * @brief 移除装饰品
+			 */
 			void remove( size_t idx ){
-				if( idx < m_dcrtes__.size() ){
-					m_dcrtes__.erase( idx );
+                if( idx < m_dcrtes__.size() ){
+                    m_dcrtes__.erase( idx );
 				}
 			}
-
-			void decratMe( std::functional< void (dcrte_t & ) > cb ){
-				size_t count = m_dcrtes__.size();
+			/**
+			 * @brief 实现具体业务的接口，在回调函数中执行装饰任务
+			 * @param cb[ I ]回调函数， void fun( decrat_t& item );
+			 *          
+			 * @note 这个接口会使用openomp的方式并行执行，如果在业务程序中存在互斥数据应该自行处理好
+			 */
+            void decratMe( std::function< void (dcrte_t & ) > cb ){
+                size_t count = m_dcrtes__.size();
 			
 				for( size_t i = 0; i < count; i ++ ){
-					cb( m_dcrtes__[ i ] );
+                    cb( m_dcrtes__[ i ] );
 				}
 			}
 
-			void decratMe( std::functional< void (itfcType * itfc ) > cb ){
-				size_t count = m_dcrtes__.size();
+            void decratMe( std::function< void (itfc_t * itfc ) > cb ){
+                size_t count = m_dcrtes__.size();
+			
 				for( size_t i = 0; i < count; i ++ ){
-					cb( m_dcrtes__[ i ].get().get() );
+                    cb( m_dcrtes__[ i ] );
 				}
 			}
 		
-			iterator begin(){ return m_dcrtes__.begin(); }
-			iterator end(){ return m_dcrtes__.end(); }
+            iterator begin(){ return m_dcrtes__.begin(); }
+            iterator end(){ return m_dcrtes__.end(); }
 		};
 	}
 }
